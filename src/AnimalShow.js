df3cd9054d7b30e20c3f5fee85a8fad4bf02e1f5
Salah Ahmed <salahibrahimismailsayed.ahmed@st.uskudar.edu.tr>
2023-03-22 12:30:00 +0300

    setup the project files

---
+++ src/AnimalShow.js
@@ -0,0 +1,5 @@
+function Animal() {
+	return <h2>We have h2 here</h2>
+}
+
+export default Animal
--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import Animal from './AnimalShow';
+
 function App() {
+	console.log(Animal)
 	return <h1>Hello there!</h1>
 }
 
